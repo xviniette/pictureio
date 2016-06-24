e3e19d625e1d7f0f15342b4a23b2e59fde5cd0a9
xviniette <xviniette@gmail.com>
2016-06-24 11:05:35 +0200

    suite

--- pictureio.js
+++ pictureio.js
@@ -3,9 +3,12 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var async = require("async");
+
 var request = require("request");
 var uuid = require('uuid');
 
+var cheerio = require('cheerio');
 
 var words = require("./server/words.js");
 
@@ -19,39 +22,75 @@ String.prototype.noAccent = function(){
         /[\331-\334]/g, /[\371-\374]/g, // U, u
         /[\321]/g, /[\361]/g, // N, n
         /[\307]/g, /[\347]/g, // C, c
-    ];
-    var noaccent = ['A','a','E','e','I','i','O','o','U','u','N','n','C','c'];
-    var str = this;
-    for(var i = 0; i < accent.length; i++){
-        str = str.replace(accent[i], noaccent[i]);
+        ];
+        var noaccent = ['A','a','E','e','I','i','O','o','U','u','N','n','C','c'];
+        var str = this;
+        for(var i = 0; i < accent.length; i++){
+            str = str.replace(accent[i], noaccent[i]);
+        }
+        return str;
     }
-    return str;
+
+    var getTraductions = function(word, target, callback){
+       var url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl="+target+"&dt=t&q="+word;
+
+       request({
+          method:"GET",
+          url:url
+      }, function(err, reponse, body){
+          if(err){
+             callback();
+         }else{
+             var value = body.split('"')[1].noAccent();
+             callback(value);
+         }
+     });
+
+   }
+
+   var getRandomPicture = function(word, callback){
+    var url = "http://www.bing.com/images/async?q="+word+"&async=content&first=0&count=10&dgst=ro_u892*&IID=images.1&SFX=2&IG=D94A4836D0054273A224A088DB6C8F4D&CW=990&CH=972&CT=1466756991818&qft=+filterui:imagesize-medium&form=R5IR2";
+
+    request({
+        method:"GET",
+        url:url
+    }, function(err, reponse, body){
+        if(err){
+            callback();
+        }else{
+            console.log(body);
+            callback(value);
+        }
+    });
 }
 
-var getTraductions = function(word, target, callback){
-	var url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl="+target+"&dt=t&q="+word;
-
-	request({
-		method:"GET",
-		url:url
-	}, function(err, reponse, body){
-		if(!err){
-			callback();
-		}else{
-			var value = body.split('"')[1];
-			callback(value);
-		}
-	});
-
-}
-
-var getRandomWord = function(callback){
+var getRandomWord = function(cb){
 	var word = words[Math.floor(Math.random() * words.length)];
+
 	var langList = ["fr", "ca"];
 
+    currentWord = [];
 
+    async.forEachOf(langList, function(value, key, callback){
+        getTraductions(word, value, function(data){
+            if(data != null){
+                currentWord.push(data);
+            }
+            callback();
+        });
+    }, function(err){
+        currentWord.push(word);
+        cb(word);
+    });
 }
 
+getRandomWord(function(word){
+    console.log(word);
+    console.log(currentWord);
+    getRandomPicture(word, function(){
+
+    });
+})
 
 
 var goodGuess = function(guess){
@@ -66,9 +105,6 @@ var goodGuess = function(guess){
 var currentWord = [];
 var startTime = 0;
 
-
-
-
 var players = {};
 var ranking = [];
 
@@ -79,40 +115,47 @@ io.on('connection', function(socket){
 		id:uuid.v1(),
 		pseudo:"Player-"+Math.floor(Math.random()*1000),
 		times:[],
-		ranks:[]
-	}
+		ranks:[],
+        lastGuess:0
+    }
 
-	socket.emit("you", players[socket.id]);
-	socket.emit("players",  Object.keys(players).map(function (key) {return players[key]}));
-	io.emit("newPlayer", players[socket.id]);
+    socket.emit("you", players[socket.id]);
+    socket.emit("players",  Object.keys(players).map(function (key) {return players[key]}));
+    io.emit("newPlayer", players[socket.id]);
 
 
-	socket.on("pseudo", function(pseudo){
-		if(pseudo.length > 0){
-			players[socket.id].pseudo = pseudo;
-			io.emit("pseudo", {id:players[socket.id].id, pseudo:players[socket.id].pseudo});
-		}
-	});
+    socket.on("pseudo", function(pseudo){
+      if(pseudo.length > 0){
+         players[socket.id].pseudo = pseudo;
+         io.emit("pseudo", {id:players[socket.id].id, pseudo:players[socket.id].pseudo});
+     }
+ });
 
-	socket.on('guess', function(word){
-		if(goodGuess(word)){
-			var p = players[socket.id];
-			ranking.push(p);
-			var time = Date.now() - startTime;
-			p.times.push(time);
-			p.ranks.push(ranking.length);
+    socket.on('guess', function(word){
+        var p = players[socket.id];
+        if(Date.now() - p.lastGuess > 500){
+            p.lastGuess = Date.now();
+            if(goodGuess(word)){
+                ranking.push(p);
+                var time = Date.now() - startTime;
+                p.times.push(time);
+                p.ranks.push(ranking.length);
 
-			socket.emit("guess", {valid:true, word:word, position:ranking.length, time:time});
-			io.emit("playerGuess", {players:p, position:ranking.length, time:time});
-		}else{
-			socket.emit("guess", {valid:false, word:word});
-		}
-	});
+                socket.emit("guess", {valid:true, word:word, position:ranking.length, time:time});
+                io.emit("playerGuess", {players:p, position:ranking.length, time:time});
+            }else{
+                socket.emit("guess", {valid:false, word:word});
+            }
+        }else{
+            socket.emit("toofast");
+        }
 
-	socket.on('disconnect', function(){
-		io.emit("removePlayer", players[socket.id]);
-		delete players[socket.id];
-	});
+    });
+
+    socket.on('disconnect', function(){
+      io.emit("removePlayer", players[socket.id]);
+      delete players[socket.id];
+  });
 });
 
 app.use(express.static("public"));
