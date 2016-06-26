0d94a9d63a03d373a2a9ec288b608b50b454cfad
xviniette <xviniette@gmail.com>
2016-06-27 01:23:39 +0200

    update

--- pictureio.js
+++ pictureio.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var fs = require('fs');
 
 var async = require("async");
 
@@ -12,178 +13,40 @@ var cheerio = require('cheerio');
 
 var words = require("./server/words.js");
 
-var startTime = 0;
-
-var currentWord = [];
-
-var players = {};
-var ranking = [];
-
-
-String.prototype.noAccent = function(){
-  var accent = [
-        /[\300-\306]/g, /[\340-\346]/g, // A, a
-        /[\310-\313]/g, /[\350-\353]/g, // E, e
-        /[\314-\317]/g, /[\354-\357]/g, // I, i
-        /[\322-\330]/g, /[\362-\370]/g, // O, o
-        /[\331-\334]/g, /[\371-\374]/g, // U, u
-        /[\321]/g, /[\361]/g, // N, n
-        /[\307]/g, /[\347]/g, // C, c
-        ];
-        var noaccent = ['A','a','E','e','I','i','O','o','U','u','N','n','C','c'];
-        var str = this;
-        for(var i = 0; i < accent.length; i++){
-          str = str.replace(accent[i], noaccent[i]);
-        }
-        return str;
-      }
-
-      var getTraductions = function(word, target, callback){
-       var url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl="+target+"&dt=t&q="+word;
-
-       request({
-        method:"GET",
-        url:url
-      }, function(err, reponse, body){
-        if(err){
-         callback();
-       }else{
-         var value = body.split('"')[1].noAccent().toUpperCase();
-         callback(value);
-       }
-     });
-
-     }
-
-     var getRandomPicture = function(word, callback){
-      var url = "https://www.google.fr/search?q="+word+"&tbm=isch&ijn=1";
-      request({
-        method:"GET",
-        url:url
-      }, function(err, reponse, body){
-        if(err){
-          callback();
-        }else{
-          $ = cheerio.load(body);
-          var images = $(".rg_l");
-          var urls = [];
-          for(var i in images){
-            if(images[i].attribs && images[i].attribs.href && images[i].attribs.href.split("imgurl=")[1] && images[i].attribs.href.split("imgurl=")[1].split("&")[0]){
-              urls.push(images[i].attribs.href.split("imgurl=")[1].split("&")[0]);
-            }
-          }
-          callback(urls[Math.floor(Math.random() * urls.length)]);
-        }
-      });
-    }
-
-    
-
-    var getRandomWord = function(cb){
-     var word = words[Math.floor(Math.random() * words.length)];
-
-     var langList = ["fr", "ca"];
-
-     currentWord = [];
-
-     async.forEachOf(langList, function(value, key, callback){
-      getTraductions(word, value, function(data){
-        if(data != null){
-          currentWord.push(data);
-        }
-        callback();
-      });
-    }, function(err){
-      currentWord.push(word.noAccent().toUpperCase());
-      cb(word);
-    });
-   }
-
-   var goodGuess = function(guess){
-     for(var i in currentWord){
-      if(currentWord[i] == guess.noAccent().toUpperCase()){
-       return true;
-     }
-   }	
-   return false;
- }
-
- var newRound = function(){
-  getRandomWord(function(word){
-    for(var i in players){
-      players[i].hasFound = false;
-    }
-    startTime = Date.now();
-    ranking = [];
-    getRandomPicture(word, function(url){
-      io.emit("newRound", {words:currentWord, img:url});
-    });
-    
-  });
+var noAccent = function(str){
+	var accent = [
+	/[\300-\306]/g, /[\340-\346]/g,
+	/[\310-\313]/g, /[\350-\353]/g, 
+	/[\314-\317]/g, /[\354-\357]/g, 
+	/[\322-\330]/g, /[\362-\370]/g, 
+	/[\331-\334]/g, /[\371-\374]/g, 
+	/[\321]/g, /[\361]/g, 
+	/[\307]/g, /[\347]/g, 
+	];
+	var noaccent = ['A','a','E','e','I','i','O','o','U','u','N','n','C','c'];
+	for(var i = 0; i < accent.length; i++){
+		str = str.replace(accent[i], noaccent[i]);
+	}
+	return str;
 }
 
+eval(fs.readFileSync('./server/Game.js')+'');
 
-setInterval(function(){
-  newRound();
-}, 10 * 1000);
+var game = new Game();
 
-newRound();
-
-
+game.start();
 
 
 io.on('connection', function(socket){
+	game.addPlayer(socket);
 
-	players[socket.id] = {
-		id:uuid.v1(),
-		pseudo:"Player-"+Math.floor(Math.random()*10000),
-		times:[],
-		ranks:[],
-    lastGuess:0,
-    hasFound:false
-  }
+	socket.on('guess', function(word){
+		game.suggestion(socket, word);
+	});
 
-  socket.emit("you", players[socket.id]);
-  socket.emit("players",  Object.keys(players).map(function (key) {return players[key]}));
-  socket.broadcast.emit("newPlayer", players[socket.id]);
-
-  socket.on("pseudo", function(pseudo){
-    if(pseudo.length > 0){
-     players[socket.id].pseudo = pseudo;
-     io.emit("pseudo", {id:players[socket.id].id, pseudo:players[socket.id].pseudo});
-   }
- });
-
-  socket.on('guess', function(word){
-    var p = players[socket.id];
-    if(p.hasFound){
-      socket.emit("alreadyFound");
-      return;
-    }
-    if(Date.now() - p.lastGuess > 500){
-      p.lastGuess = Date.now();
-      if(goodGuess(word)){
-        ranking.push(p);
-        var time = Date.now() - startTime;
-        p.times.push(time);
-        p.ranks.push(ranking.length);
-
-        socket.emit("guess", {valid:true, word:word, position:ranking.length, time:time});
-        p.hasFound = true;
-        io.emit("playerGuess", {player:p, position:ranking.length, time:time});
-      }else{
-        socket.emit("guess", {valid:false, word:word});
-      }
-    }else{
-      socket.emit("toofast");
-    }
-
-  });
-
-  socket.on('disconnect', function(){
-    io.emit("removePlayer", players[socket.id]);
-    delete players[socket.id];
-  });
+	socket.on('disconnect', function(){
+		game.removePlayer(socket);
+	});
 });
 
 app.use(express.static("public"));
---
+++ server/Game.js
@@ -0,0 +1,167 @@
+var Game = function(){
+	this.players = {};
+	this.nextPosition = 1;
+
+	this.currentWords = [];
+	this.picture = null;
+
+	this.startTime = 0;
+	this.roundTime = 30 * 1000;
+
+	this.languages = ["fr"];
+}
+
+Game.prototype.start = function(){
+	var _this = this;
+
+	this.currentWords = [];
+	this.nextPosition = 1;
+
+	for(var i in this.players){
+		this.players[i].hasFound = false;
+	}
+
+	this.getRandomWord(function(word, words){
+		_this.currentWords = words;
+		_this.getRandomPicture(word, function(img){
+			_this.picture = img;
+			_this.startTime = Date.now();
+			console.log(word, img);
+			io.emit("newGame", {timeleft:_this.roundTime, img:_this.picture, words:_this.currentWords});
+		});
+	});
+
+	setTimeout(function(){
+		_this.start();
+	}, this.roundTime);
+}
+
+
+Game.prototype.getRandomPicture = function(word, callback){
+	var url = "https://www.google.fr/search?q="+word+"&tbm=isch&ijn=1&tbs=isz:m";
+	request({
+		method:"GET",
+		url:url
+	}, function(err, reponse, body){
+		if(err){
+			callback();
+		}else{
+			$ = cheerio.load(body);
+			var images = $(".rg_l");
+			var urls = [];
+			for(var i in images){
+				if(images[i].attribs && images[i].attribs.href && images[i].attribs.href.split("imgurl=")[1] && images[i].attribs.href.split("imgurl=")[1].split("&")[0]){
+					urls.push(images[i].attribs.href.split("imgurl=")[1].split("&")[0]);
+				}
+			}
+			callback(urls[Math.floor(Math.random() * urls.length)]);
+		}
+	});
+}
+
+Game.prototype.getTraductions = function(word, target, callback){
+	var url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl="+target+"&dt=t&q="+word;
+	request({
+		method:"GET",
+		url:url
+	}, function(err, reponse, body){
+		if(err){
+			callback();
+		}else{
+			callback(body.split('"')[1]);
+		}
+	});
+}
+
+Game.prototype.getRandomWord = function(cb){
+	var word = words[Math.floor(Math.random() * words.length)];
+	var allWords = [];
+	var _this = this;
+
+	async.forEachOf(this.languages, function(value, key, callback){
+		_this.getTraductions(word, value, function(data){
+			if(data != null){
+				allWords.push({
+					language:value,
+					word:data,
+					clean:noAccent(data).toUpperCase()
+				});
+			}
+			callback();
+		});
+	}, function(err){
+		allWords.push({
+			language:"en",
+			word:word,
+			clean:noAccent(word).toUpperCase()
+		});
+		cb(word, allWords);
+	});
+}
+
+Game.prototype.goodSuggestion = function(word){
+	for(var i in this.currentWords){
+		var splitWord = this.currentWords[i].clean.split(" ");
+		var splitGuess = word.split(" ");
+		for(var w of splitWord){
+			for(var g of splitGuess){
+				if(w == noAccent(g).toUpperCase()){
+					return this.currentWords[i];
+				}
+			}
+		}
+	}	
+	return false;
+}
+
+Game.prototype.suggestion = function(socket, word){
+	var p = this.players[socket.id];
+	if(p.hasFound){
+		socket.emit("alreadyFound");
+		return;
+	}
+	if(Date.now() - p.lastGuess > 500){
+		p.lastGuess = Date.now();
+
+		var isGood = this.goodSuggestion(word);
+		if(isGood){
+			var time = Date.now() - this.startTime;
+			p.times.push(time);
+			p.ranks.push(this.nextPosition);
+
+			socket.emit("guess", {valid:true, word:isGood, position:this.nextPosition, time:time});
+			io.emit("playerGuess", {player:p, position:this.nextPosition, time:time});
+			p.hasFound = true;
+			this.nextPosition++;
+		}else{
+			socket.emit("guess", {valid:false, word:word});
+		}
+	}else{
+		socket.emit("toofast");
+	}
+}	
+
+Game.prototype.addPlayer = function(socket){
+	this.players[socket.id] = {
+		id:uuid.v1(),
+		pseudo:"Player-"+Math.floor(Math.random()*10000),
+		times:[],
+		ranks:[],
+		lastGuess:0,
+		hasFound:false
+	};
+
+	socket.emit("you", this.players[socket.id]);
+	socket.emit("players", this.getPlayers());
+	socket.broadcast.emit("newPlayer", this.players[socket.id]);
+}
+
+Game.prototype.removePlayer = function(socket){
+	io.emit("removePlayer", this.players[socket.id]);
+	delete this.players[socket.id];
+}
+
+Game.prototype.getPlayers = function(){
+	var _this = this;
+	return Object.keys(_this.players).map(function (key) {return _this.players[key]})
+}
